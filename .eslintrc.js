ba63f73ada4f9df54b05b741c1d3c1d121f0ee9c
Kazuichi Takashiro <takashiro@qq.com>
2021-02-10 11:36:49 +0800

    Setup jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,57 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
+	env: {
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		'airbnb-base',
+		'plugin:@typescript-eslint/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: [
+		'@typescript-eslint',
+	],
+	rules: {
+		'class-methods-use-this': 'off',
+		indent: [
+			'error',
+			'tab',
+		],
+		'linebreak-style': 'off',
+		'import/extensions': [
+			'error',
+			'ignorePackages',
+			{
+				ts: 'never',
+				js: 'never',
+			},
+		],
+		'max-len': [
+			'error',
+			300,
+		],
+		'no-await-in-loop': 'off',
+		'no-continue': 'off',
+		'no-plusplus': 'off',
+		'no-param-reassign': 'off',
+		'no-restricted-syntax': [
+			'error',
+			'WithStatement',
+		],
+		'no-tabs': 'off',
+	},
+	settings: {
+		'import/resolver': {
+			node: {
+				extensions: [
+					'.ts',
+					'.js',
+				],
+			},
+		},
+	},
 };
